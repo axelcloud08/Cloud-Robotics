d4a224cb35ca2fb9aaad7ce770b86045109c6784
Lya4444 <marthalopezresearch@gmail.com>
2025-07-06 16:26:21 -0600

    Update Arduino_master.ino

--- Arduino_master.ino
+++ Arduino_master.ino
@@ -7,9 +7,9 @@
 #include "Temporizador.h"
 #include "Encoders.h"
 
-Temporizador EncodersPrint(100);
-Encoder left(18,19);
-Encoder rigth(21,20);
+Temporizador EncodersPrint(50);
+Encoder left(19,18);
+Encoder rigth(20,21);
 MiniMaestro maestro(Serial2);
 Motors motor_driver(0,1);
 char direction;
@@ -44,13 +44,13 @@ void rigth_reading(){
   rigth.ticks_count += (c1_status == c2_status) ? 1 : -1;
 }
 
-Servo back_flipper1(0, 2224, 1008, 1648, 3);
-Servo back_flipper2(1, 2224, 1008, 1648, 3);
-Servo front_flipper1(2, 2224, 1008, 1648, 3);
-Servo front_flipper2(3, 2224, 1008, 1648, 3);
-Servo base(4, 2224, 1527, 2224, 10);
-Servo codo(5, 2224, 1008, 1008, 10);
-Servo cuello(6, 2224, 1008, 1648, 10);
+Servo back_flipper1(0, 2224, 1008, 1648, 5);
+Servo back_flipper2(1, 2224, 1008, 1648, 5);
+Servo front_flipper1(2, 2200, 1008, 1648, 5);
+Servo front_flipper2(3, 2200, 1008, 1648, 5);
+Servo base(4, 2000, 1084, 1084, 10);
+Servo codo(5, 2050, 1500, 1008, 15);
+Servo cuello(11, 2224, 1008, 1616, 10);
 Servo garra(7, 2224,1008, 2224, 10);
 
 
@@ -90,6 +90,8 @@ void setup() {
   Serial.begin(9600);
   Serial2.begin(115200);
   Serial.setTimeout(10);
+  ServosIniciar();
+  //Serial.println("conexion establecida");
 
   pinMode(left.pin1, INPUT_PULLUP);
   pinMode(left.pin2, INPUT_PULLUP);
@@ -98,7 +100,6 @@ void setup() {
 
   attachInterrupt(digitalPinToInterrupt(left.pin1), left_reading, CHANGE);
   attachInterrupt(digitalPinToInterrupt(rigth.pin1), rigth_reading, CHANGE);
-  ServosIniciar();
 }
 
 void loop() {
@@ -108,7 +109,6 @@ ServosUpdate();
     EncodersPrint.reiniciar();
     String message = String(millis()) + "," +  String(left.ticks_count) + "," + String(rigth.ticks_count);
     Serial.println(message);
-    
   }
 
   if (Serial.available()) {
@@ -119,11 +119,11 @@ ServosUpdate();
 
      switch(direction){
 
-      case 'w':
+      case 's':
       motor_driver.front(speed);
       break;
 
-      case 's':
+      case 'w':
       motor_driver.back(speed);
       break;
 
@@ -151,19 +151,19 @@ ServosUpdate();
       break;
 
       case 't':
-      front_flipper1.go_toMAX();
-      break;
-
-      case 'g':
-      front_flipper1.go_toMIN();
-      break;
-
-      case 'y':
       front_flipper2.go_toMIN();
       break;
 
-      case 'h':
+      case 'g':
       front_flipper2.go_toMAX();
+      break;
+
+      case 'y':
+      front_flipper1.go_toMAX();
+      break;
+
+      case 'h':
+      front_flipper1.go_toMIN();
       break;
 
       case 'q':
@@ -184,13 +184,14 @@ ServosUpdate();
       break;
 
       case 'u':      
-      base.go_toMIN();
-      codo.go_toPrepos(1438);
+      base.go_toMAX();
+      codo.go_toPrepos(1839);
       break;
 
       case 'n':      
-      codo.go_toMIN();
-      base.go_toMAX();
+      codo.go_toPrepos(1008);
+      base.go_toMIN();
+      cuello.go_toPrepos(1616);
       break;
     
       case 'i':
